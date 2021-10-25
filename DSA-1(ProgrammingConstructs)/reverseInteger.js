ea5abae861ab6c78f6910868e0373b355e7a573e
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-10-24 22:04:53 -0400

    DSA-1 setup

---
+++ DSA-1(ProgrammingConstructs)/reverseInteger.js
@@ -0,0 +1,135 @@
+/* 
+Problem Description
+Given a number, return the reverse of that number.
+
+Input format
+One line of input containing an integer N, the number to be reversed.
+
+Output format
+Return the reversed number. Note that the number must start with (1-9) digits only unless it is a single digit number.
+
+Sample Input 1
+15
+
+Sample Output 1
+51
+
+Constraints
+0 <= N <= 1000000
+
+Explanation
+Reverse of 15 is 51.
+
+Resource
+Reverse Digits of a Number
+Crio Methodology - Problem Approach
+Brainstorm some approaches for solving the problem before viewing the video
+
+video: https://youtu.be/LGghl1GN9k4 
+Crio Methodology - Problem Coding
+Try coding on your own before checking the below video
+
+Checklist
+
+How to reverse a number?
+
+
+What is the time complexity of this solution?
+
+
+Reverse of number 0?
+
+
+Reverse of number 10000?
+
+
+Reverse of number 202?
+
+Follow up
+
+What if n is 1000000009?
+
+
+What if n has 1000 digits?
+*/
+
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.replace(/\s+/g, " ").trim();
+    });
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function readIntArr() {
+  let str = readLine();
+  str = str.split(" ");
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    arr.push(parseInt(str[i], 10));
+  }
+  return arr;
+}
+
+function print(x) {
+  process.stdout.write(x + "");
+}
+
+// TODO: Implement this method
+function reverseNum(n) {
+    return n.toString().split("").reverse().join("") * Math.sign(n);
+}
+
+console.log(reverseNum(3456));
+console.log(reverseNum(3-124-6));
+// NOTE: Please do not modify this function
+function main() {
+  let n = parseInt(readLine(), 10);
+  let result = reverseNum(n);
+  console.log(result);
+}
+
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
