fcc1c37e6cbe150ec10fab03c4a89e8d75d0ddee
Dionne Noella Barretto <denverdionne@gmail.com>
2021-12-11 19:11:40 -0500

    is alphabet

---
+++ FE2- ES6 Practice Problems/higherOrderFuncs.js
@@ -0,0 +1,126 @@
+/* 
+Problem Description
+Given a character X you need to find whether X is an alphabet or not.
+
+If yes, return a string "Yes" or otherwise throw an error with the message "Not Alphabet".
+
+
+Please use the following code snippet to check if X is an alphabet or not.
+
+
+
+function checkAlphabet(X) {
+
+	let n = X.charCodeAt(0);
+
+	let strStartsWithALetter = (n>=65 && n < 91) || ( n>=97 && n<123);
+
+	
+
+// You can Google about what is ASCII code to know more about
+
+	// why are we using numbers like 65, 91, 97 & 123
+
+	
+
+if (strStartsWithALetter) {
+
+	// Yes, it starts with an alphabet
+
+	return true;
+
+} else {
+
+	// No, it doesn’t start with an alphabet
+
+	return false;
+
+} 
+
+}
+
+References
+(MDN) JavaScript: throw
+
+JavaScript String functions
+
+ASCII Table
+
+https://www.w3schools.com/js/js_comparisons.asp
+
+Input
+You are given character X.
+
+Output
+Return string "Yes" or throw the error as per problem statement.
+
+Sample input 1
+A
+
+Sample output 1
+Yes
+
+Sample input 2
+0
+
+Sample output 2
+Function should throw an error with the message "Not Alphabet".
+
+
+Solution : https://www.youtube.com/watch?v=9J_punOhWlM
+*/
+function isAlphabet(X) {
+  if (checkAlphabet(X)) {
+    return "Yes";
+  } else {
+    throw "Not Alphabet";
+  }
+}
+
+function checkAlphabet(X) {
+  let n = X.charCodeAt(0);
+
+  let strStartsWithALetter = (n >= 65 && n < 91) || (n >= 97 && n < 123);
+
+  // You can Google about what is ASCII code to know more about
+
+  // why are we using numbers like 65, 91, 97 & 123
+
+  if (strStartsWithALetter) {
+    // Yes, it starts with an alphabet
+
+    return true;
+  } else {
+    // No, it doesnt start with an alphabet
+
+    return false;
+  }
+}
+
+module.exports = isAlphabet;
+/* 
+Crio Methodology
+
+Milestone 1: Understand the problem clearly
+1. Ask questions & clarify the problem statement clearly.
+2. Take an example or two to confirm your understanding of the input/output & extend it to test cases
+Milestone 2: Finalize approach & execution plan
+1. Understand what type of problem you are solving.
+     a. Obvious logic, tests ability to convert logic to code
+     b. Figuring out logic
+     c. Knowledge of specific domain or concepts
+     d. Knowledge of specific algorithm
+     e. Mapping real world into abstract concepts/data structures
+2. Brainstorm multiple ways to solve the problem and pick one
+3. Get to a point where you can explain your approach to a 10 year old
+4. Take a stab at the high-level logic & write it down.
+5. Try to offload processing to functions & keeping your main code small.
+Milestone 3: Code by expanding your pseudocode
+1. Make sure you name the variables, functions clearly.
+2. Avoid constants in your code unless necessary; go for generic functions, you can use examples for your thinking though.
+3. Use libraries as much as possible
+Milestone 4: Prove to the interviewer that your code works with unit tests
+1. Make sure you check boundary conditions
+2. Time & storage complexity
+3. Suggest optimizations if applicable
+*/
